e5cadea6dab39acfc70ed9650c63268eec6b5a39
Prinz Preince <prinzpreince@live.com>
2023-08-30 20:10:08 +0200

    initializr

---
+++ src/test/java/com/projects/SpringbootInPractice/SpringbootInPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.SpringbootInPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
