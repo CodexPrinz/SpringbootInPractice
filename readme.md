f51bd5caf6ddb31f3742e0b01a3404fd46c114aa
Prinz Preince <prinzpreince@live.com>
2023-10-09 20:23:41 +0200

    CommandLineRunner as implementation

--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 Spring Boot in Practice
 SOMNATH MUSIB
 Foreword by JOSH LONG
+
+
+## Executing code on Spring Boot application startup
+The **CommandLineRunner** and **ApplicationRunner** are two Spring Boot interfaces
+that provide a single run(..) method and are invoked just before a Spring Boot application
+finishes its initialization.
--- src/main/java/com/projects/SpringbootInPractice/SpringbootInPracticeApplication.java
+++ src/main/java/com/projects/SpringbootInPractice/SpringbootInPracticeApplication.java
@@ -1,13 +1,22 @@
 package com.projects.SpringbootInPractice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootInPracticeApplication {
+public class SpringbootInPracticeApplication implements CommandLineRunner {
+
+	protected static final Logger logger = LoggerFactory.getLogger(SpringbootInPracticeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootInPracticeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+	}
 }
