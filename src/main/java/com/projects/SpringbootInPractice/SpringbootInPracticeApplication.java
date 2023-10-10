2d472dd9f9f1db721dc7ee869dd712f9991ef534
Prinz Preince <prinzpreince@live.com>
2023-10-10 21:37:12 +0200

    validator

--- src/main/java/com/projects/SpringbootInPractice/SpringbootInPracticeApplication.java
+++ src/main/java/com/projects/SpringbootInPractice/SpringbootInPracticeApplication.java
@@ -1,13 +1,35 @@
 package com.projects.SpringbootInPractice;
 
+import com.projects.SpringbootInPractice.model.Course;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootInPracticeApplication {
+import java.util.Set;
 
+@SpringBootApplication
+public class SpringbootInPracticeApplication implements CommandLineRunner {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringbootInPracticeApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootInPracticeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Course course = new Course();
+		course.setId(1);
+		course.setRating(0);
+		Validator validator= Validation.buildDefaultValidatorFactory().getValidator();
+
+		Set<ConstraintViolation<Course>> violations = validator.validate(course);
+
+		violations.forEach(courseConstraintViolation -> logger.error("A constraint violation has occurred. Violation " +
+				"details: [{}].", courseConstraintViolation));
+	}
 }
---
+++ src/main/java/com/projects/SpringbootInPractice/model/Course.java
@@ -0,0 +1,69 @@
+package com.projects.SpringbootInPractice.model;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+public class Course {
+
+    private long id;
+    private String name;
+    private String category;
+    @Min(value = 1, message = "A course should have a minimum of 1 rating")
+    @Max(value = 5, message = "A course should have a maximum of 5 rating")
+    private int rating;
+    private String description;
+
+    public Course() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
